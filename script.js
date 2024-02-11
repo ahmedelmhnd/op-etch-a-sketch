4e0c047f739d3d8b571325ec0ec0bed773e446da
Ahmed A <ahmed.elmohanned@gmail.com>
2024-02-11 22:29:32 +0800

    added resize feature and randomized color

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const contianer = document.querySelector(".grid-container")
 
-
+const resizeButton = document.querySelector(".resize");
 let gridSize = 16;
 
-for (let i = 0; i < gridSize; i++) 
+resizeButton.addEventListener("click", () => 
+{
+    clearGrid();
+    gridSize = prompt("Please choose a new size. (range: 1:100)");
+    buildGrid(gridSize);
+});
+
+
+buildGrid(gridSize);
+
+
+
+function buildGrid(gridSize) 
+{
+    for (let i = 0; i < gridSize; i++) 
 {
     let row = document.createElement("div");
+    row.classList.add("rows");
     row.style.cssText = "flex: 1; display: flex; flex-direction: column;";
     contianer.appendChild(row);
     for (let i = 0; i < gridSize; i++) 
@@ -16,14 +31,25 @@ for (let i = 0; i < gridSize; i++)
         row.appendChild(pixel);
         pixel.addEventListener("mouseenter", () => 
         {
-            pixel.style.cssText += "background-color: red;"
+            let  r = getRandomInt(256);
+            let g = getRandomInt(256);
+            let b = getRandomInt(256);
+            pixel.style.cssText += "background-color: rgb(" + r + "," + g + "," + b + ");";
         });
     }
     
 }
+}
 
-/*const pixels = document.querySelectorAll(pixel);
-pixels.forEach(element => 
+function clearGrid() 
 {
-    element.addEventListener("mouseenter", (event) => {});
-});*/
+    const rows = document.querySelectorAll(".rows");
+
+    rows.forEach(element => {
+        element.remove();
+    });
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
